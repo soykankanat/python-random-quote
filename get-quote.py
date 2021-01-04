4824aa3f5edc425b080f8bf17de1f615d31dde31
soykan kanat <soykankanat@gmail.com>
2021-01-04 17:31:16 +0300

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,8 @@
-def soykan():
-  print("Keep it logically awesome.")
+import random
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-
-  print(quotes)
-
-if __name__== "__main__":
-  soykan()
+f = open("quotes.txt")
+quotes = f.readlines()
+f.close()
+last = 13
+rnd = random.randint(0,last)
+print(quotes[rnd])
